e61c0cb84c768b08f8a70ca0b7c37ddc2c36cc0b
Adlai Holler <guy.on.internet@icloud.com>
2016-04-30 15:45:27 -0700

    Add example project

---
+++ SwiftKittens/SwiftKittens/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftKittens
+//
+//  Created by Adlai Holler on 4/30/16.
+//  Copyright © 2016 Adlai Holler. All rights reserved.
+//
+
+import AsyncDisplayKit
+import AsyncDisplayKitExtensions
+
+class ViewController: ASViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
