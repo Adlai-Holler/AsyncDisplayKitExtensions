09500bbbd81c30cd750bd41d143eb2e4194df432
Adlai Holler <guy.on.internet@icloud.com>
2016-04-30 14:32:42 -0700

    Initial Commit

---
+++ AsyncDisplayKitExtensions/AsyncDisplayKitExtensions.h
@@ -0,0 +1,19 @@
+//
+//  AsyncDisplayKitExtensions.h
+//  AsyncDisplayKitExtensions
+//
+//  Created by Adlai Holler on 4/30/16.
+//  Copyright © 2016 Adlai Holler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncDisplayKitExtensions.
+FOUNDATION_EXPORT double AsyncDisplayKitExtensionsVersionNumber;
+
+//! Project version string for AsyncDisplayKitExtensions.
+FOUNDATION_EXPORT const unsigned char AsyncDisplayKitExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncDisplayKitExtensions/PublicHeader.h>
+
+
